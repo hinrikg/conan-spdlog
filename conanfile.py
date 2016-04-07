ea5ddfa1aea714c6a2898a0d4dc6c7c2295e2234
Hinrik Gylfason <hinrikg@gmail.com>
2016-04-07 22:29:19 +0000

    :space_invader: hello world

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conans import CMake, ConanFile
+from conans.tools import get
+
+
+class SpdlogConan(ConanFile):
+    name = 'spdlog'
+    version = 'master'
+    author = 'Hinrik Gylfason (hinrikg@gmail.com)'
+    url = 'http://github.com/hinrikg/conan-spdlog'
+    license = 'MIT'
+
+    settings = None
+    generators = 'cmake'
+
+    def source(self):
+        get('https://github.com/gabime/spdlog/archive/master.zip')
+
+    def package(self):
+        self.copy('*.h', dst='include', src='spdlog-master/include', keep_path=True)
+        self.copy('*.cc', dst='include', src='spdlog-master/include', keep_path=True)
+        self.copy('LICENSE', dst='.', src='spdlog-master')
