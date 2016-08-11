41e9508d4865ea637599fa0e4264e26523c89dec
Hinrik Gylfason <hinrikg@gmail.com>
2016-08-11 12:03:46 +0000

    feat: package spdlog version 0.9.0 instead of master

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans.tools import get
 
 class SpdlogConan(ConanFile):
     name = 'spdlog'
-    version = 'master'
+    version = '0.9.0'
     author = 'Hinrik Gylfason (hinrikg@gmail.com)'
     url = 'http://github.com/hinrikg/conan-spdlog'
     license = 'MIT'
@@ -13,9 +13,9 @@ class SpdlogConan(ConanFile):
     generators = 'cmake'
 
     def source(self):
-        get('https://github.com/gabime/spdlog/archive/master.zip')
+        get('https://github.com/gabime/spdlog/archive/v0.9.0.zip')
 
     def package(self):
-        self.copy('*.h', dst='include', src='spdlog-master/include', keep_path=True)
-        self.copy('*.cc', dst='include', src='spdlog-master/include', keep_path=True)
-        self.copy('LICENSE', dst='.', src='spdlog-master')
+        self.copy('*.h', dst='include', src='spdlog-0.9.0/include', keep_path=True)
+        self.copy('*.cc', dst='include', src='spdlog-0.9.0/include', keep_path=True)
+        self.copy('LICENSE', dst='.', src='spdlog-0.9.0')
